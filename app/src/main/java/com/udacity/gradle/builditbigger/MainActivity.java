a0d03cc278a6376e752fb2a6ef1a37003722bcf6
Peter Arcentales <peterarcentales@gmail.com>
2017-12-19 01:22:49 -0500

    Step 1 is completed. Also we have updated the gradle version and other dependencies.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.gradleandroidlibrary.peterarct.jokejavalibrary.JokeSupplier;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        String joke = JokeSupplier.getTestJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ jokeJavaLibrary/src/main/java/com/gradleandroidlibrary/peterarct/jokejavalibrary/JokeSupplier.java
@@ -0,0 +1,9 @@
+package com.gradleandroidlibrary.peterarct.jokejavalibrary;
+
+public class JokeSupplier {
+
+    public static String getTestJoke(){
+        return "This is a joke from the Java Library...";
+    }
+
+}

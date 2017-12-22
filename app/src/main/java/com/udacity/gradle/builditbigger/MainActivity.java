d9a4915583ee406db7cedbb499edd0f8625d3141
Peter Arcentales <peterarcentales@gmail.com>
2017-12-22 01:26:35 -0500

    Progress Indicator was added when retrieving jokes. App is ready to be tested in an emulator.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity
-                                    implements LoaderManager.LoaderCallbacks<String>{
+                                    {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,70 +45,10 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            getSupportLoaderManager().restartLoader(0,null,this);
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void tellJoke(View view) {
-        //String joke = JokeSupplier.getTestJoke();
-        //JokeDisplayActivity.launch(this,joke);
-    }
-
-
-    @Override
-    public android.support.v4.content.Loader<String> onCreateLoader(int i, Bundle bundle) {
-        return new AsyncTaskLoader<String>(MainActivity.this) {
-
-            private MyApi myApiService = null;
-
-            @Override
-            protected void onStartLoading() {
-                super.onStartLoading();
-                forceLoad();
-            }
-
-            @Override
-            public String loadInBackground() {
-                Context context = MainActivity.this;
-
-
-                if(myApiService == null) {  // Only do this once
-                    MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                            new AndroidJsonFactory(), null)
-                            // options for running against local devappserver
-                            // - 10.0.2.2 is localhost's IP address in Android emulator
-                            // - turn off compression when running against local devappserver
-                            .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                            .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                                @Override
-                                public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                    abstractGoogleClientRequest.setDisableGZipContent(true);
-                                }
-                            });
-                    // end options for devappserver
-
-                    myApiService = builder.build();
-                }
-
-                try {
-                    return myApiService.tellRandomJoke().execute().getData();
-                } catch (IOException e) {
-                    return e.getMessage();
-                }
-            }
-        };
-    }
-
-    @Override
-    public void onLoadFinished(android.support.v4.content.Loader<String> loader, String data) {
-        Toast.makeText(this,data,Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onLoaderReset(android.support.v4.content.Loader<String> loader) {
-
     }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,19 +1,34 @@
 package com.udacity.gradle.builditbigger;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+import com.udacity.gradle.builditbigger.databinding.FragmentMainBinding;
+
+import java.io.IOException;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<String>{
+
+    private FragmentMainBinding mBinding;
 
     public MainActivityFragment() {
     }
@@ -21,16 +36,79 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
-        mAdView.loadAd(adRequest);
-        return root;
+        mBinding.adView.loadAd(adRequest);
+
+        // Set OnClickListener when user clicks in Tell joke.
+        mBinding.tellJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLoaderManager().restartLoader(0,null,MainActivityFragment.this);
+            }
+        });
+
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        return new AsyncTaskLoader<String>(getActivity()) {
+
+            @Override
+            protected void onStartLoading() {
+                super.onStartLoading();
+                mBinding.loadingJokeContainer.setVisibility(View.VISIBLE);
+                forceLoad();
+            }
+
+            @Override
+            public String loadInBackground() {
+
+                MyApi myApiService = null;
+
+                if(myApiService == null) {  // Only do this once
+                    MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                            new AndroidJsonFactory(), null)
+                            // options for running against local devappserver
+                            // - 10.0.2.2 is localhost's IP address in Android emulator
+                            // - turn off compression when running against local devappserver
+                            .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                            .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                                @Override
+                                public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                    abstractGoogleClientRequest.setDisableGZipContent(true);
+                                }
+                            });
+                    // end options for devappserver
+
+                    myApiService = builder.build();
+                }
+
+                try {
+                    return myApiService.tellRandomJoke().execute().getData();
+                } catch (Exception e) {
+                    return e.getMessage();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        mBinding.loadingJokeContainer.setVisibility(View.GONE);
+        Toast.makeText(getActivity(),data,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
     }
 }
